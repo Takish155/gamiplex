c5dd5b1d8392c169bd3c17f4081dfe697759e257
Takish Lim <rimutakeshi155@gmail.com>
2024-01-02 00:14:51 +0900

    removed logout button from searchinput component

--- src/Header/SearchInput.tsx
+++ src/Header/SearchInput.tsx
@@ -6,9 +6,8 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Box, Button, useMediaQuery } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import { searchInputPaper } from "./_headerStyle";
-import { signOut, useSession } from "next-auth/react";
 
 const SearchInput = ({
   type,
@@ -20,7 +19,6 @@ const SearchInput = ({
   const [input, setInput] = useState("");
   const router = useRouter();
   const matches = useMediaQuery("(max-width:750px)");
-  const session = useSession();
 
   return (
     <Box
@@ -59,9 +57,6 @@ const SearchInput = ({
           </IconButton>
         </>
       </Paper>
-      {session.status === "authenticated" && (
-        <Button onClick={() => signOut()}>Sign out</Button>
-      )}
     </Box>
   );
 };
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { mainPageStyle } from "../auth/_authStyle";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <Box sx={mainPageStyle}>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          a
+        </Grid>
+        <Grid item xs={12} md={6}>
+          i
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default page;
